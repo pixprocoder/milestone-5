979045bceee1ee99d529474e51232e9274bfe340
Samsul kobir <89704576+skpixels@users.noreply.github.com>
2022-12-09 02:33:59 +0600

    dom explored

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-console.log("hello world");
+const friends = document.getElementsByTagName("li");
+
+for (const friend of friends) {
+  friend.style.backgroundColor = "blue";
+  friend.style.margin = "20px";
+  //   friend.style.padding = "20px";
+  //   friend.style.color = "white";
+  //   friend.style.fontSize = "2rem";
+  //   friend.style.fontSize = "2rem";
+}
+
+const blogs = document.getElementsByTagName("p");
+blogs[3].innerText = "hello world";
+blogs[3].style.backgroundColor = "green";
+
+const spacialBlog = document.getElementById("blog-spacial");
+
+const classBlog = document.getElementsByClassName("class-blog");
+// console.log("classBlog", classBlog);
+
+// const querySelect = document.querySelector(".class-blog");
+// querySelect.style.backgroundColor = "blue";
+// console.log("this is for query", querySelect);
+
+const querySelectAll = document.querySelectorAll(".class-blog");
+
+for (const arr of querySelectAll) {
+  console.log(arr);
+}
